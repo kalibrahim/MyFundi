56a88dedb5d3983820a3158cd1ad04e2822e05b3
kalibrahim <Kal@Khalids-MacBook-Pro.local>
2017-10-02 13:55:36 -0700

    Added a feed UI

--- MyFundi/View/CircleView.swift
+++ MyFundi/View/CircleView.swift
@@ -1,23 +1,28 @@
 //
-//  FancyView.swift
+//  CircleView.swift
 //  MyFundi
 //
-//  Created by Khalid Al Ibrahim on 9/30/17.
+//  Created by Khalid Al Ibrahim on 10/2/17.
 //  Copyright © 2017 Bachmanity. All rights reserved.
 //
 
 import UIKit
 
-class FancyView: UIView {
+class CircleView: UIImageView {
 
     override func awakeFromNib() {
         super.awakeFromNib()
-        
         layer.shadowColor = UIColor(displayP3Red: SHADOW_GRAY, green: SHADOW_GRAY, blue: SHADOW_GRAY, alpha: 0.6).cgColor
         layer.shadowOpacity = 0.8
         layer.shadowRadius = 5.0
         layer.shadowOffset = CGSize(width: 1.0, height: 1.0)
-        layer.cornerRadius = 2.0
     }
+    
+    override func draw(_ rect: CGRect) {
+        super.draw(rect)
+        layer.cornerRadius = self.frame.width / 2
+    }
+    
+    
 
 }
