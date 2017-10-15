ac0c7106e2ba5b16941e924cf96b6c96ff1c5efe
Joseph Ishak <jishak1@asu.edu>
2017-10-15 11:50:17 -0700

    Profile started with a Succesfull segue from Feed To Profile

---
+++ MyFundi/Controller/FeedVC.swift
@@ -0,0 +1,99 @@
+//
+//  FeedVC.swift
+//  MyFundi
+//
+//  Created by Khalid Al Ibrahim on 10/1/17.
+//  Copyright © 2017 Bachmanity. All rights reserved.
+//
+
+import UIKit
+import SwiftKeychainWrapper
+import Firebase
+
+class FeedVC: UIViewController, UITableViewDelegate, UITableViewDataSource, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var addImage: CircleView!
+    
+    var posts = [Post]()
+    var imagePicker: UIImagePickerController!
+    static var imageCache: NSCache<AnyObject, UIImage> = NSCache()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+        imagePicker = UIImagePickerController()
+        imagePicker.allowsEditing = true
+        imagePicker.delegate = self
+        
+        DataService.ds.REF_FUNDRAISERS.observe(.value, with: { (snapshot) in
+            if let snapshot = snapshot.children.allObjects as? [DataSnapshot] {
+                for snap in snapshot {
+                    print("SNAP: \(snap)")
+                    if let postDict = snap.value as? Dictionary<String, AnyObject> {
+                        let key = snap.key
+                        let post = Post(postKey: key, postData: postDict)
+                        self.posts.append(post)
+                    }
+                }
+            }
+            self.tableView.reloadData()
+        })
+    }
+    
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return posts.count
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let post = posts[indexPath.row]
+        
+        if let cell = tableView.dequeueReusableCell(withIdentifier: "PostCell") as? PostCell {
+
+            if let img = FeedVC.imageCache.object(forKey: post.imageUrl as AnyObject) {
+                cell.configureCell(post: post, img: img)
+                return cell
+            } else {
+                cell.configureCell(post: post)
+                return cell
+            }
+        } else {
+            return PostCell()
+        }
+    }
+
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        if let image = info[UIImagePickerControllerEditedImage] as? UIImage {
+            addImage.image = image
+        } else {
+            print("KHALID: A valid image wasnt selected")
+        }
+        imagePicker.dismiss(animated: true, completion: nil)
+    }
+    
+    
+    @IBAction func addImageTapped(_ sender: AnyObject) {
+        present(imagePicker, animated: true, completion: nil)
+    }
+    
+    @IBAction func profileImageTapped(_ sender: Any) {
+        performSegue(withIdentifier: "goToProfFromFeed", sender: nil)
+    }
+    
+    @IBAction func signOutTapped(_ sender: AnyObject) {
+        let keychainResult = KeychainWrapper.standard.removeObject(forKey: KEY_UID)
+        print("KHALID: ID removed from keychain \(keychainResult)")
+        try! Auth.auth().signOut()
+        performSegue(withIdentifier: "goToSignIn", sender: nil)
+    }
+}
---
+++ MyFundi/View/ProfileHistoryCell.swift
@@ -0,0 +1,19 @@
+//
+//  ProfileHistoryCell.swift
+//  MyFundi
+//
+//  Created by Joseph  Ishak on 10/15/17.
+//  Copyright © 2017 Bachmanity. All rights reserved.
+//
+
+import UIKit
+
+class ProfileHistoryCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+
+}
