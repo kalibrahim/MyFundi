977565e2128a8d996b77ee621de9498318930631
Joseph Ishak <jishak1@asu.edu>
2017-10-15 12:26:44 -0700

    Succesfully implemented configureCell Function in Profile History Cell View

--- MyFundi/View/ProfileHistoryCell.swift
+++ MyFundi/View/ProfileHistoryCell.swift
@@ -10,9 +10,24 @@ import UIKit
 import Firebase
 class ProfileHistoryCell: UITableViewCell {
 
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var amountLabel: UILabel!
+    
+    var post: Post!
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
+    }
+    
+    func configureCell(post: Post, img: UIImage? = nil) {
+        self.post = post
+        self.titleLabel.text = post.title
+        self.amountLabel.text = "\(post.donationGoal)"
+        dateLabel.text = "11/11/2011"
+        
+        
     }
 
 
