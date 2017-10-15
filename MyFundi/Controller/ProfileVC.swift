fc3aef0e43e8b02bf3b8d245649515285d4a1fda
Joseph Ishak <jishak1@asu.edu>
2017-10-15 12:30:46 -0700

    Succesfully completed the Table View for the Profile.

--- MyFundi/Controller/ProfileVC.swift
+++ MyFundi/Controller/ProfileVC.swift
@@ -13,8 +13,9 @@ import SwiftKeychainWrapper
 class ProfileVC: UIViewController,  UITableViewDelegate, UITableViewDataSource, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
     @IBOutlet weak var tableView: UITableView!
-   var imagePicker: UIImagePickerController!
+    var imagePicker: UIImagePickerController!
     
+     var posts = [Post]()
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -23,6 +24,20 @@ class ProfileVC: UIViewController,  UITableViewDelegate, UITableViewDataSource, 
         imagePicker = UIImagePickerController()
         imagePicker.allowsEditing = true
         imagePicker.delegate = self
+        
+        DataService.ds.REF_FUNDRAISERS.observe(.value, with: { (snapshot) in
+            if let snapshot = snapshot.children.allObjects as? [DataSnapshot] {
+                for snap in snapshot {
+                    print("Joe: Profile \(snap)")
+                    if let postDict = snap.value as? Dictionary<String, AnyObject> {
+                        let key = snap.key
+                        let post = Post(postKey: key, postData: postDict)
+                        self.posts.append(post)
+                    }
+                }
+            }
+            self.tableView.reloadData()
+        })
         
     }
 
@@ -34,10 +49,19 @@ class ProfileVC: UIViewController,  UITableViewDelegate, UITableViewDataSource, 
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 1
+        return posts.count
     }
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-       return ProfileHistoryCell()
+        let post = posts[indexPath.row]
+        
+        if let cell = tableView.dequeueReusableCell(withIdentifier: "ProfileHistoryCell") as? ProfileHistoryCell {
+            
+                cell.configureCell(post: post)
+                return cell
+            
+        } else {
+            return PostCell()
+        }
     }
     @IBAction func signOutTapped(_ sender: Any) {
         let keychainResult = KeychainWrapper.standard.removeObject(forKey: KEY_UID)
